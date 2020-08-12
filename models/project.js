353f0011ca1b504828680d39cc2b8109b13a5d1f
Rome Bell <romebell5@gmail.com>
2020-08-12 10:56:27 -0700

    [solution] Upload part 1 and part 2

---
+++ models/project.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const project = sequelize.define('project', {
+    name: DataTypes.STRING,
+    githubLink: {
+      type: DataTypes.TEXT,
+      validate: {
+        isUrl: true
+      }
+    },
+    deployLink: {
+      type: DataTypes.TEXT,
+      validate: {
+        isUrl: true
+      }
+    },
+    description: DataTypes.TEXT
+  }, {})
+
+  project.associate = function(models) {
+    // associations can be defined here
+    models.project.belongsToMany(models.category, 
+      { through: 'categories_projects', onDelete: 'CASCADE' });
+  }
+
+  return project
+}
